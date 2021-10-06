4f662ad2213cc44bbf6b0a2773ac1be5ce332e8f
Dasha <dashuta.tkachenko@list.ru>
2021-10-06 22:52:57 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 2000000000;
+        int addSum = 500000000;
+        int newSum = sum + addSum;
+        System.out.println(newSum);
+    }
+}
